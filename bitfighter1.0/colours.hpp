1e6c1344d98deb5929aefa4ae02d462fd78d12e6
Alex Barry <alex.barry@gmail.com>
2012-05-16 13:28:18 -0400

    Removed legacy class, SDL_color works just as well, added helper defines in colours.hpp (because I'm canadian)

---
+++ bitfighter1.0/colours.hpp
@@ -0,0 +1,47 @@
+
+#ifndef bitfighter_colours_hpp
+#define bitfighter_colours_hpp	1
+
+#define red			255,0,0
+#define green		0,255,0
+#define blue		0,0,255
+#define yellow		255,255,0
+#define cyan		0,255,255
+#define magenta		255,0,255
+#define black		0,0,0
+#define white		255,255,255
+
+#define gray20		51,51,51
+#define gray40		102,102,102
+#define gray50		128,128,128
+#define gray70		179,179,179
+#define gray80		204,204,204
+
+#define yellow40	102,102,0
+#define orange50	255,128,0
+#define orange67	255,171,0
+
+#define paleRed		255,128,128
+#define paleGreen	128,255,128
+#define paleBlue	128,128,255
+#define palePurple	255,128,255
+
+#define red30		77,0,0
+#define red35		89,0,0
+#define red40		102,0,0
+#define red50		128,0,0
+#define red60		153,0,0
+#define red80		204,0,0
+
+#define richGreen	0,89,0
+#define green65		0,166,0
+#define green80		0,204,0
+
+#define blue40		0,0,102
+#define blue80		0,0,204
+
+#define menuHelpColor					green
+#define idlePlayerScoreboardColor		gray50
+#define standardPlayerScoreboardColor	white
+
+#endif /* bitfighter_colours_hpp */
